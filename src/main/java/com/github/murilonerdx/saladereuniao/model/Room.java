d029705782764267945b2ca07b59a19b3b6152fe
Murilonerdx <mu-silva@outlook.com>
2021-06-18 20:05:10 -0300

    Arrumando h2-console

--- src/main/java/com/github/murilonerdx/saladereuniao/model/Room.java
+++ src/main/java/com/github/murilonerdx/saladereuniao/model/Room.java
@@ -21,35 +21,4 @@ public class Room {
     private String startHour;
     private String endHour;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStartHour() {
-        return startHour;
-    }
-
-    public void setStartHour(String startHour) {
-        this.startHour = startHour;
-    }
-
-    public String getEndHour() {
-        return endHour;
-    }
-
-    public void setEndHour(String endHour) {
-        this.endHour = endHour;
-    }
 }
