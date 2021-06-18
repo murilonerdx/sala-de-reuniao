8ac55080f856da824705e64f7f37ba18d802b030
Murilonerdx <mu-silva@outlook.com>
2021-06-18 19:53:58 -0300

    Finalizando projeto

--- src/main/java/com/github/murilonerdx/saladereuniao/controller/RoomController.java
+++ src/main/java/com/github/murilonerdx/saladereuniao/controller/RoomController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value="/api/v1/rooms")
+@RequestMapping(value="/api/v1/")
 public class RoomController {
 
     @Autowired
--- src/main/java/com/github/murilonerdx/saladereuniao/model/Room.java
+++ src/main/java/com/github/murilonerdx/saladereuniao/model/Room.java
@@ -20,4 +20,36 @@ public class Room {
     private String name;
     private String startHour;
     private String endHour;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(String startHour) {
+        this.startHour = startHour;
+    }
+
+    public String getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(String endHour) {
+        this.endHour = endHour;
+    }
 }
