c561de05b03af3ee1328d7b139cc640c35bd9661
Murilonerdx <mu-silva@outlook.com>
2021-06-18 19:43:16 -0300

    Adicionando rota

--- src/main/java/com/github/murilonerdx/saladereuniao/controller/RoomController.java
+++ src/main/java/com/github/murilonerdx/saladereuniao/controller/RoomController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value="")
+@RequestMapping(value="/api/v1/rooms")
 public class RoomController {
 
     @Autowired
